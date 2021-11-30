35cd288e6d0d3d39c44b73ea3962dc6fe2dd1337
Vladislav Rusakov <v.rusakov@q-pol.ru>
2021-11-30 18:04:17 +0300

    End

---
+++ loading_wrapper.py
@@ -0,0 +1,38 @@
+import itertools
+import threading
+import time
+import sys
+
+
+def loading(function):
+    def wrapper():
+        done = False
+
+        def animate():
+            SPEED = 2
+            LINE = '\rLoading '
+
+            for frame in itertools.cycle(
+                    ["⡏ ", "⡗ ", "⡧ ", "⣇ ", "⣸ ", "⢼ ", "⢺ ", "⢹ "]):
+                if done:
+                    break
+                sys.stdout.write(LINE + frame)
+                sys.stdout.flush()
+                time.sleep(SPEED / 10)
+            sys.stdout.write('\rDone!' + ' ' * (len(LINE) + 2))
+
+        thread = threading.Thread(target=animate)
+        thread.start()
+
+        function()
+
+        done = True
+    return wrapper
+
+
+@loading
+def sleeps():
+    time.sleep(5)
+
+
+sleeps()
